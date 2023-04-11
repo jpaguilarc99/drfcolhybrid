5237012395d2612bb9595bfe430b6fdbb1b830f3
Juan Pablo <jpaguilarc99@gmail.com>
2023-04-10 21:11:08 -0500

    automating createsuperuser

--- mainapi/build.sh
+++ mainapi/build.sh
@@ -8,5 +8,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py makemigrations
-python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
